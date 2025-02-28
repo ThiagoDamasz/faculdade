cb8bffd06f893cbeeb5d8e5993ead5c2a0c0a323
Thiago Damas Ferreira Silva <118909770+ThiagoDamasz@users.noreply.github.com>
2025-02-28 14:31:10 -0300

    Delete S01/Lua/Exercício1.lua

--- S01/Lua/Exercício1.lua
+++
@@ -1,8 +0,0 @@
-numero = io.read()
-numero = tonumber(numero)
-
-local contador = 1
-while contador <= 10 do
-  print(numero .. " * " .. contador .. "=" .. numero * contador)
-  contador = contador + 1
-end
