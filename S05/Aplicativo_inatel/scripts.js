2a1eda00b61f1187f990841dce284b802869a412
Thiago Damas <thiago.damas@ges.inatel.br>
2025-03-10 20:41:47 -0300

    Atividade completa

--- S05/Aplicativo_inatel/scripts.js
+++ S05/Aplicativo_inatel/scripts.js
@@ -1,3 +1,22 @@
 function openMenu(){
-    document.getElementById()
+    document.getElementById("menu").style.display = "block"
+}
+
+function closeMenu(){
+    document.getElementById("menu").style.display = "none"
+}
+
+function temaEscuro(){
+    document.documentElement.style.setProperty('--cor-icone', '#4E20A2')
+    document.documentElement.style.setProperty('--cor-back1', '#162133')
+    document.documentElement.style.setProperty('--cor-texto', 'white')
+    document.documentElement.style.setProperty('--cor-sombra', '#D9C5A0')
+   
+}
+
+function temaClaro(){
+    document.documentElement.style.setProperty('--cor-icone', '#4970BF')
+    document.documentElement.style.setProperty('--cor-back1', '#FAFCFB')
+    document.documentElement.style.setProperty('--cor-texto', 'black')
+    document.documentElement.style.setProperty('--cor-sombra', 'black')
 }
